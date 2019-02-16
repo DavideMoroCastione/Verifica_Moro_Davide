f0fff32a13a1dfe44cd90c311bdfa3f9886f7d92
Davide <davide.moro@barsanti.edu.it>
2019-02-16 08:39:23 +0100

    16/02/2019 - Verifica Moro_Davide

--- sketch_feb16a/sketch_feb16a.ino
+++ sketch_feb16a/sketch_feb16a.ino
@@ -1,10 +1,16 @@
 
 int temporosso;
+int tempogiallo;
+int tempobianco;
+int tempoverde;
 
 void setup() {
   // put your setup code here, to run once:
 
   temporosso = 0;
+  tempogiallo = 0;
+  tempobianco = 0;
+  tempoverde = 0;
   
   pinMode(13, OUTPUT); //rosso
   pinMode(11, OUTPUT); //giallo
@@ -30,5 +36,24 @@ void TempoRosso() {
 void TempoGiallo() {
 
   Serial.begin(9600);
+  Serial.print("Quanto resta acceso il giallo?");
+  while(Serial.available() == 0) {};
+  tempogiallo = Serial.readString().toInt();
+}
+
+void TempoBianco() {
+
+  Serial.begin(9600);
+  Serial.print("Quanto resta acceso il bianco?");
+  while(Serial.available() == 0) {};
+  tempobianco = Serial.readString().toInt();
+}
+
+void TempoVerde() {
+
+  Serial.begin(9600);
+  Serial.print("Quanto resta acceso il verde?");
+  while(Serial.available() == 0) {};
+  tempoverde = Serial.readString().toInt();
 }
 
